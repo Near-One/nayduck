c5dad336e8fd548ab636d5e7364408e928126bb4
Maria <skidanova.maria@gmail.com>
2020-08-07 16:15:45 -0700

    Requirements for the infra

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# nayduck
+# NayDuck
 Test Infra
---
+++ requirements/updates.sh
@@ -0,0 +1,20 @@
+sudo apt update -y
+sudo apt install python3.7
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+sudo update-alternatives --config python3
+sudo apt-get install --reinstall python3-apt
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g npx
+npm install --save react-router-dom
+sudo apt-get install python3-dev
+sudo apt install libpython3.7-dev
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+sudo apt-get install clang
+sudo apt-get install cmake
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+sudo update-alternatives --config python
+sudo apt-get -y install python3-pip
