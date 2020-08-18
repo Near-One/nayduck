bbaf418d8af40315866474b9f7660b047e87e49e
Maria <skidanova.maria@gmail.com>
2020-08-18 16:00:44 -0700

    Passing pipe responses

--- requirements/updates.sh
+++ requirements/updates.sh
@@ -1,21 +1,22 @@
-sudo apt update -y
-sudo apt install python3.7
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-sudo update-alternatives --config python3
-sudo apt-get install --reinstall python3-apt
-sudo apt-get install nodejs
-sudo apt-get install npm
-sudo npm install -g npx
-npm install --save react-router-dom
-sudo apt-get install python3-dev
-sudo apt install libpython3.7-dev
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-rustup target add wasm32-unknown-unknown
-sudo apt-get install clang
-sudo apt-get install cmake
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
-sudo update-alternatives --config python
-sudo apt-get -y install python3-pip
+set -x
+echo -ne '\n' | sudo apt update -y
+echo -ne '\n' | sudo apt install python3.7
+echo -ne '\n' | sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+echo -ne '\n' | sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+echo -ne '\n' | sudo update-alternatives --config python3
+echo -ne '\n' | sudo apt-get install --reinstall python3-apt
+echo -ne '\n' | sudo apt-get install nodejs
+echo -ne '\n' | sudo apt-get install npm
+echo -ne '\n' | sudo npm install -g npx
+echo -ne '\n' | npm install --save react-router-dom
+echo -ne '\n' | sudo apt-get install python3-dev
+echo -ne '\n' | sudo apt install libpython3.7-dev
+echo -ne '\n' | curl https://sh.rustup.rs -sSf | sh | echo -ne '1\n'  
+echo -ne '\n' | source $HOME/.cargo/env
+echo -ne '\n' | rustup target add wasm32-unknown-unknown
+echo -ne '\n' | sudo apt-get install clang
+echo -ne '\n' | sudo apt-get install cmake
+echo -ne '\n' | sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+echo -ne '\n' | sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+echo -ne '\n' | sudo update-alternatives --config python
+echo -ne '\n' | sudo apt-get -y install python3-pip
