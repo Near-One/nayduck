1efd98862804a34bb58efa5fe59b1669109abd73
Michal Nazarewicz <mina86@mina86.com>
2021-07-07 00:18:00 +0200

    scheduler: refactor `request_a_run` endpoint
    Add a helper function `request_a_run_impl` which uses a new Failure
    exception to indicate any kind of failure.  This simplifies the early
    returns as construction of an response object and jsonification of it
    is not repeated over and over.
    
    Furthermore, change a few conditions to make the code more concise and
    reduce amount of indentation.
    
    Overall, this does not affect behaviour of the code.
    
    Issue: https://github.com/near/nayduck/issues/6

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,48 +1,59 @@
-from flask import Flask, session, flash, render_template, redirect, json, url_for, request, abort, make_response, jsonify, send_file
-from rc import bash, ok
-import requests
 import os
+import typing
+import traceback
+
+import requests
+import flask
+from rc import bash
 
 from db import SchedulerDB
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/request_a_run', methods=['POST', 'GET'])
-def request_a_run():
-    request_json = request.get_json(force=True)
+
+class Failure(Exception):
+    """An exception indicating failure of the request_a_run request."""
+
+    def __init__(self, response: typing.Any) -> None:
+        super().__init__('Failure. {}'.format(response))
+
+    def to_response(self) -> typing.Dict[str, typing.Union[int, str]]:
+        """Returns a JSON object intended to return to the caller on failure."""
+        return {'code': 1, 'response': self.args[0]}
+
+
+def request_a_run_impl(request_json: typing.Dict[str, typing.Any]) -> int:
+    """Starts a test run based on the JSON request.
+
+    Args:
+        request_json: The JSON object describing the request client is making.
+    Returns:
+        Numeric identifier of the scheduled test run.
+    Raises:
+        Failure: on any kind of error.
+    """
     # if not request_json['token']:
-    #     resp = {'code': 1, 'response': 'Failure. Your client is too old. NayDuck requires Github auth. Sync your client to head.'}
-    #     return jsonify(resp)
+    #     raise Failure('Your client is too old. NayDuck requires Github auth. '
+    #                   'Sync your client to head.')
     server = SchedulerDB()
     if 'token' in request_json:
         github_login = server.get_github_login(request_json['token'])
         if not github_login:
-            resp = {'code': 1, 'response': 'Failure. NayDuck token is not found. Do not try to fake it.'}
-            return jsonify(resp)
-        elif github_login == "NayDuck":
-                allowed = True
-        else:
-            allowed = False
+            raise Failure('NayDuck token is not found. Do not try to fake it.')
+        if github_login != 'NayDuck':
             github_req = f'''https://api.github.com/users/{github_login}/orgs'''
             response = requests.get(github_req)
-            for org in response.json():
-                if "login" in org and (org['login'] == 'nearprotocol' or org['login'] == 'near'):
-                    allowed = True
-                    break
-            if not allowed:
-                resp = {'code': 1, 'response': f'''Failure. {github_login} is not part of NearProtocol or Near orgs.'''}
-                return jsonify(resp)
+            if not any(org.get('login') in ('nearprotocol', 'near')
+                       for org in response.json()):
+                raise Failure(f'{github_login} is not part of '
+                              'NearProtocol or Near orgs.')
     if not request_json['branch'] or not request_json['sha']:
-        resp = {'code': 1, 'response': 'Failure. Branch and/or git sha were not provided.'}
-        return jsonify(resp)
+        raise Failure('Branch and/or git sha were not provided.')
 
-    if 'requester' in request_json:
-        requester = request_json['requester']
-    else:
-        requester = 'unknown'
-    
+    requester = request_json.get('requester', 'unknown')
+
     fetch = bash(f'''
             rm -rf nearcore
             git clone https://github.com/near/nearcore
@@ -50,37 +61,46 @@ def request_a_run():
             git fetch 
             git checkout {request_json['sha']}
     ''')
-    if fetch.returncode == 0:
-        user = bash(f'''
-            cd nearcore
-            git log --format='%ae' {request_json['sha']}^!
-        ''').stdout
-        title = bash(f'''
-            cd nearcore
-            git log --format='%s' {request_json['sha']}^!
-        ''').stdout
-        tests = []
-        for x in request_json['tests']:
-                if len(x.strip()) and x[0] != '#':
-                    spl = x.split(' ', 1)
-                    if spl[0].isnumeric():
-                        tests.extend([spl[1]] * int(spl[0]))
-                    else:
-                        tests.append(x)
-        try:
-            run_id = server.scheduling_a_run(branch=request_json['branch'],
-                                  sha=request_json['sha'],
-                                  user=user.split('@')[0],
-                                  title=title,
-                                  tests=tests,
-                                  requester=requester)
-        except Exception as e:
-            return jsonify({'code': 1, 'response': 'Failure. %s' % str(e)})
+    if fetch.returncode != 0:
+        raise Failure(fetch.stderr)
 
-        resp = {'code': 0, 'response': 'Success. ' + os.getenv('NAYDUCK_UI') + '/#/run/' + str(run_id)}
-    else:
-        resp = {'code': 1, 'response': 'Failure. ' + str(fetch.stderr)}
-    return jsonify(resp)
+    user = bash(f'''
+        cd nearcore
+        git log --format='%ae' {request_json['sha']}^!
+    ''').stdout
+    title = bash(f'''
+        cd nearcore
+        git log --format='%s' {request_json['sha']}^!
+    ''').stdout
+    tests = []
+    for test in request_json['tests']:
+        spl = test.split(maxsplit=1)
+        if spl and spl[0][0] != '#':
+            if len(spl) > 1 and spl[0].isnumeric():
+                tests.extend(spl[1:] * int(spl[0]))
+            else:
+                tests.append(test.strip())
+    return server.scheduling_a_run(branch=request_json['branch'],
+                                   sha=request_json['sha'],
+                                   user=user.split('@')[0],
+                                   title=title,
+                                   tests=tests,
+                                   requester=requester)
+
+
+@app.route('/request_a_run', methods=['POST', 'GET'])
+def request_a_run():
+    request_json = flask.request.get_json(force=True)
+    try:
+        run_id = request_a_run_impl(request_json)
+        url = '{}/#/run/{}'.format(os.getenv('NAYDUCK_UI'), run_id)
+        response = {'code': 0, 'response': 'Success. ' + url}
+    except Failure as ex:
+        response = ex.to_response()
+    except Exception as ex:
+        traceback.print_exc()
+        response = Failure(ex).to_response()
+    return flask.jsonify(response)
 
 
 if __name__ == '__main__':
