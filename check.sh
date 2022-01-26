006e3cf433dbe96e659d478b7da29321123a620b
Michal Nazarewicz <mina86@mina86.com>
2022-01-26 13:42:29 +0100

    Add `check.sh` script

---
+++ check.sh
@@ -0,0 +1,14 @@
+# A bit of a hacky script for running all the checks and formatting for the code
+# base.
+
+set -eu
+
+parallel 'sh -xc {}' ::: \
+	'exec pylint -j0 */*.py' \
+	'exec python -m yapf -pir .' \
+	'exec mypy -m backend.backend' \
+	'exec mypy -m workers.builder' \
+	'exec mypy -m workers.worker' \
+	'exec shellcheck */*.sh'
+
+exec pytest "$@"
