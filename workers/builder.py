4fe52abbec7e6741f74617d5e99c0667965deeb7
Longarithm <the.alex.logunov@gmail.com>
2024-08-27 18:11:32 +0500

    verbose

--- workers/builder.py
+++ workers/builder.py
@@ -119,6 +119,7 @@ def build_target(spec: BuildSpec, runner: utils.Runner) -> None:
 
     cargo(
         'build',
+        '-vv',
         '-pgenesis-populate',
         '-prestaked',
         '-pnear-test-contracts',
